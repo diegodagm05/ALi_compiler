00a5f4e6389dffb075a7bcc215935308ce0f1894
Diego Guerrero <diegokmc-2013@hotmail.com>
2022-10-02 19:42:52 -0500

    Added the grammar rules for expressions

--- parser.py
+++ parser.py
@@ -9,9 +9,9 @@ from lexer import tokens
 
 # ----------------------
 # GLOBAL RULES 
-def p_programa(p):
-    '''programa : global_vars_funs main
-                | main'''
+def p_program(p):
+    '''program : global_vars_funs main
+               | main'''
 
 def p_global_vars_funs(p):
     '''global_vars_funs : g_vf''' 
@@ -23,18 +23,18 @@ def p_g_vf(p):
 
 def p_g_vars(p):
     '''g_vars : vars g_vars
-                | vars'''
+              | vars'''
 
 def p_g_functions(p):
     '''g_functions : functions g_functions
-                    | functions'''
+                   | functions'''
 
 def p_main(p):
     '''main : VOID FUNC MAIN ( ) { vars s }
             | VOID FUNC MAIN ( ) { }'''
 
 # ----------------------
-# STATMENTS RULES 
+# STATEMENTS RULES 
 
 def p_vars(p):
     '''vars : VAR ids : type ; vars
@@ -42,11 +42,11 @@ def p_vars(p):
 
 def p_s(p):
     '''s : statements s
-            | statements'''
+         | statements'''
 
 def p_ids(p):
     '''ids : ID , ids
-            | ID'''
+           | ID'''
 
 def p_type(p):
     '''type : INT 
@@ -57,7 +57,7 @@ def p_type(p):
 
 def p_array_type(p):
     '''array_type : ARRAY [ expression ]
-                    | ARRAY [ expression ] [ expression ]'''
+                  | ARRAY [ expression ] [ expression ]'''
 
 def p_functions(p):
     '''functions : return_function functions
@@ -74,41 +74,41 @@ def p_p(p):
 
 def p_content(p):
     '''content : vars s
-                | empty'''
+               | empty'''
 
 def p_ret(p):
     '''ret : ID
-            | expression'''
+           | expression'''
 
 def p_void_function(p):
     '''void_function : VOID FUNC ID ( p ) { content }'''
 
 def p_statements(p):
     '''statements : assigment
-                    | call_to_fun ;
-                    | write
-                    | conditionals
-                    | cycles
-                    | read'''
+                  | call_to_fun ;
+                  | write
+                  | conditionals
+                  | cycles
+                  | read'''
 
 def p_conditionals(p):
     '''conditionals : IF ( expression ) interior_block cond_in_p ELSE interior_block'''
 
 def p_cond_in_p(p):
     '''cond_in_p : cond_in
-                | empty'''
+                 | empty'''
 
 def p_cond_in(p):
     '''cond_in : ELSE IF ( expression ) interior_block cond_in
-                | ELSE IF ( expression ) interior_block'''
+               | ELSE IF ( expression ) interior_block'''
 
 def p_interior_block(p):
     '''interior_block : { }
-                        | { s }'''
+                      | { s }'''
 
 def p_params(p):
     '''params : ID : type , params
-                | ID : type'''
+              | ID : type'''
 
 def p_assignment(p):
     '''assignment : ID = expression ;
@@ -119,25 +119,25 @@ def p_write(p):
 
 def p_write_p(p):
     '''write_p : write_param , write_p 
-                | write_param'''
+               | write_param'''
 
 def p_write_param(p):
     '''write_param : STRING_CONST
-                    | ID'''
+                   | ID'''
 
 def p_read(p):
     '''read : READ ( read_p ) ;'''
 
 def p_read_p(p):
     '''read_p : STRING_CONST , read_p
-                | STRING_CONST'''
+              | STRING_CONST'''
 
 def p_call_to_fun(p):
     '''call_to_fun : ID ( )
-                    | ID ( call_p )'''
+                   | ID ( call_p )'''
 def p_call_p(p):
     '''call_p : expression , call_p
-                | expression'''
+              | expression'''
 
 def p_cycles(p):
     '''cycles : while
@@ -145,7 +145,7 @@ def p_cycles(p):
 
 def p_array_indexing(p):
     '''array_indexing : ID [ expression ]
-                        | ID [ expression ] [ expression ]'''
+                      | ID [ expression ] [ expression ]'''
 
 def p_while(p):
     '''while : WHILE ( expression ) interior_block'''
@@ -156,6 +156,54 @@ def p_for(p):
 # ----------------------
 # EXRPESSIONS RULES 
 
+def p_expression(p):
+    '''expression : expression_p '''
+
+def p_expression_p(p):
+    '''expression_p : ex
+                    | ex AND expression_p
+                    | ex OR expression_p'''
+
+def p_ex(p):
+    '''ex : exp
+          | exp op exp
+          | ! exp'''
+
+def p_op(p):
+    '''op : >
+          | <
+          | GREATER_EQ
+          | LESS_EQ
+          | EQUAL
+          | DIFFERENT'''
+
+def p_exp(p):
+    '''exp : term
+           | term op_1 term'''
+
+def p_op_1(p):
+    '''op_1 : +
+            | -'''
+
+def p_term(p):
+    '''term : factor
+            | factor op_2 factor'''
+
+def p_op_2(p):
+    '''op_2 : *
+            | /'''
+
+def p_factor(p):
+    '''factor : ( expression ) 
+              | ID
+              | array_indexing
+              | call_to_fun
+              | constants'''
+
+def p_constants(p):
+    '''constants : I_CONST
+                 | F_CONST
+                 | C_CONST'''
 
 # ----------------------
 # EMPTY & ERROR RULES 
