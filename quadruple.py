ee81173ef4a7f86db678e999a388ea2514c2b635
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-10-16 22:01:26 -0500

    Adds to quadruple data structure

--- quadruple.py
+++ quadruple.py
@@ -1,12 +1,26 @@
 from semantic_cube import operations
 
+quadruple_operations = operations | {
+    'print': 13,
+    'read': 14,
+    'write': 15,
+    'goto': 16,
+    'gotot': 17,
+    'gotof': 18
+}
+
 class Quadruple():
     # Note that operators and result are memory addresses
-    def __init__(self, op_code: int, operator1: int, operator2: int, result: int) -> None:
-        if op_code not in operations:
+    # -1 on operator2 is for operators such as assignment
+    # None on result is for quadruples that may be generated with a pending result
+    def __init__(self, operation: str, operator1: int, operator2: int = -1, result: int = None) -> None:
+        if operation not in quadruple_operations:
             raise Exception('Unkown operation on quadruple')
-        self.op_code = op_code
+        self.op_code = quadruple_operations[operation]
         self.operator1 = operator1
         self.operator2 = operator2
+        self.result = result
+
+    def fill_result(self, result: int) -> None:
         self.result = result
     
