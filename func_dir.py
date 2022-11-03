38460842565a08795b3cc07f0533f4ec8777f6a2
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-11-02 18:30:59 -0600

    Changes to func dir data structure

--- func_dir.py
+++ func_dir.py
@@ -1,13 +1,28 @@
+from numpy import number
 from vars_table import VarsTable
+from semantic_cube import types
 
+class FuncDirEntry():
+    params_list : list
+    resources : tuple
+    starts_at : number
+    vars_table : VarsTable
+    type : str
+    def __init__(self, type: str) -> None:
+        self.vars_table = VarsTable()
+        self.type = type
 class FuncDir():
-    func_dir = {
-        'global': VarsTable(),
-        'main': VarsTable(),
-    }
+    func_dir : dict[str, FuncDirEntry] = {}
 
-    def create_scope(self, scopeID: str) -> None:
-        self.func_dir[scopeID] = VarsTable()
+    def __init__(self) -> None:
+        self.create_scope('global')
+        self.create_scope('main')
 
-    def get_scope(self, scopeID: str) -> VarsTable:
-        return self.func_dir[scopeID]
+    def get_func_dir(self) -> dict[str, FuncDirEntry]:
+        return self.func_dir
+
+    def create_scope(self, scopeID: str, type: str) -> None:
+        self.func_dir[scopeID] = FuncDirEntry(type=type)
+
+    def get_scope_var_table(self, scopeID: str) -> VarsTable:
+        return self.func_dir[scopeID].vars_table
